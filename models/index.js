7bb3d5786b374a1445a7e52eae8a9343f6006ed1
Corrinne W <corrinneworden@gmail.com>
2021-05-11 21:46:51 -0400

    troubleshooting

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const User = require('./User');
-const Entry = require('./Entry');
-const Comment = require('./Comment');
+const User = require('./User.js');
+const Entry = require('./Entry.js');
+const Comment = require('./Comment.js');
 
 User.hasMany(Entry, {
     foreignKey: 'user_id',
